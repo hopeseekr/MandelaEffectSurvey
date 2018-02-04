a4c8cf1ec0bf696c5ecb447142f075272887d59d
Theodore R. Smith <theodore@phpexperts.pro>
2018-02-04 01:03:32 -0600

    Removed unnecessary docker images. Uses phpexpertsinc/docker.

--- docker/web/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# this has to be done at runtime, because the /etc/hosts file is replaced at startup by Docker
-echo '127.0.0.1 api.mandelaeffectsurvey.local' >> /etc/hosts
-
-supervisord -n
